804628eddbc2dc832814eb4011f7d56b9521300b
Hemil <hemilpatel41196@gmail.com>
2021-02-28 15:58:56 +1100

    56 module4, list Persons

--- module4/src/App.js
+++ module4/src/App.js
@@ -51,30 +51,14 @@ class App extends Component {
 
     if (this.state.showPersons) {
       persons = (
-        <div>
-            {/* Chotu  */}
-            <Person
-              name={this.state.persons[0].name}
-              age={this.state.persons[0].age}
-            />
-            {/* Daxeel */}
-            <Person
-              name={this.state.persons[1].name}
-              age={this.state.persons[1].age}
-              change={this.nameChangeHandler}
-            />
-            {/* Hemil */}
-            <Person
-              name={this.state.persons[2].name}
-              age={this.state.persons[2].age}
-              click={this.switchNameHandler.bind(this, "Hemil from Binding")}
-            >
-              My hobby is sex
-            </Person>
+        <div> 
+          {this.state.persons.map(person => {
+            return <Person name = {person.name} age={person.age}/> 
+          })} 
           </div>
       )
     }
-    
+
     return (
       <div className="App">
         <h1>Hey I am creating my first react App</h1>
